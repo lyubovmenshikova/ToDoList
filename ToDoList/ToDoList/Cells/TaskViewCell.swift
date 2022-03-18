dafd1314c2164aab89f848a20eb9e32b8a279325
lyubovmenshikova <dyachenko.lubov@yandex.ru>
2022-03-18 16:59:13 +0300

    Add interface(table,cell)

---
+++ ToDoList/ToDoList/Cells/TaskViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskViewCell.swift
+//  ToDoList
+//
+//  Created by Lyubov Menshikova on 18.03.2022.
+//
+
+import UIKit
+
+class TaskViewCell: UITableViewCell {
+
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+}
